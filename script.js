80c4625e3724e84e78333f048aa82c267d46bf72
3ddy1337 <em@2mrk.de>
2024-04-11 00:49:08 +0200

    add comments for readability

--- script.js
+++ script.js
@@ -2,61 +2,79 @@
 const inputBox = document.getElementById("input-field");
 const addButton = document.getElementById("add-button");
 const todoList = document.getElementById("todo-list");
+const all = document.getElementById("all");
+const open = document.getElementById("open");
+const done = document.getElementById("done");
 
+// State
 let state = {
   todos: [],
 };
 
+// Render
 function render() {
+  // Emptys list
   todoList.innerHTML = "";
 
+  // For each Obj in state.todos =>
   state.todos.forEach((todo) => {
+    // Create <input>
     const input = document.createElement("input");
+    // Add type = checkbox
     input.type = "checkbox";
-    input.name = "done";
+    // Assign the value of done to the checkbox
     input.checked = todo.done;
+    // Add name = done
+    input.name = "done";
 
+    // Event Listener for change on checkbox
     input.addEventListener("change", () => {
+      // Listen and change done and assign to input.checked in todo scope
       todo.done = input.checked;
+      // Save todo scope to local storage
       localStorage.setItem("state", JSON.stringify(state));
-
+      // Restart render
       render();
     });
 
+    // Create <label>
     const label = document.createElement("label");
-    const todoText = document.createTextNode(todo.description);
-    label.append(input, todoText);
 
+    // Push <input> and todo.description into <label>
+    label.append(input, todo.description);
+
+    // Create <li>
     const listItem = document.createElement("li");
+
+    // Push <label> into <li>
     listItem.append(label);
 
+    // Push <li> into <ul>
     todoList.append(listItem);
   });
 }
 
-//Event Listener for addButton & Enter
-addButton.addEventListener("click", addNewTodo);
-inputBox.addEventListener("keydown", (e) => {
-  if (e.key === "Enter") {
-    addNewTodo();
-  }
-});
-
+// Add new todo
 function addNewTodo() {
+  // Remove spaces
   const description = inputBox.value.trim();
 
+  // Stop function if string is empty
   if (description === "") {
     alert("Please insert description");
     return;
   }
 
+  // Stop function if duplicates
   for (let i = 0; i < state.todos.length; i++) {
     const todo = state.todos[i];
     if (todo.description.toLowerCase() === description.toLowerCase()) {
       alert("Todo already exists");
+      return;
     }
   }
 
+  // Create new todo
   if (description !== "") {
     const id = Date.now();
     state.todos.push({ id, description, done: false });
@@ -69,8 +87,18 @@ function addNewTodo() {
   }
 }
 
+//Event Listener for add-button & Enter
+addButton.addEventListener("click", addNewTodo);
+inputBox.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    addNewTodo();
+  }
+});
+
+// Load State from storage
 function loadStateFromStorage() {
   const loadedState = localStorage.getItem("state");
+  // Stop if null
   if (loadedState === null) {
     return;
   }
